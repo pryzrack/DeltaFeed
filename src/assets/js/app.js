355e52cdff774ab053ec57b5180cb091d2653f48
pryzrack <adrianfr96@nauta.cu>
2023-01-04 16:39:50 -0500

    initial version.

---
+++ src/assets/js/app.js
@@ -0,0 +1,78 @@
+const BOTS = {
+    nv: [ // navigation bots
+        "adb_bot1@testrun.org",
+        "simplebot@systemli.org"
+    ],
+    fd: [ // feed bots
+        "feedsbot@hispanilandia.net"
+    ],
+    tr: [ // translate bots
+        "translator@hispanilandia.net"
+    ]
+};
+function $(id){
+    return document.getElementById(id);
+};
+DEFAULT = {
+    nv: BOTS.nv[0], // navigation
+    fd: BOTS.fd[0], // feeds
+    tr: BOTS.tr[0] // translator
+};
+if (window.localStorage.getItem('deltafeed')){
+    DEFAULT = JSON.parse(window.localStorage.getItem('deltafeed-config'))
+};
+function setBot(){
+    window.localStorage.setItem('deltafeed', true);
+    window.localStorage.setItem(
+        'deltafeed-config',
+        JSON.stringify(
+            {
+                nv: $('nv').value,
+                fd: $('fd').value,
+                tr: $('tr').value
+            }
+        )
+    );
+};
+function mailto(dest, body){
+    return `mailto:${dest}?body=/${encodeURI(body)}`;
+};
+function translate(from="es", to="en", text){
+    return mailto(DEFAULT.tr, `/tr ${from} ${to} ${text}`);
+};
+function subscribe(feed, unsub=true) {
+    return mailto(DEFAULT.fd, `${unsub?'': 'un'}sub ${feed}`);
+};
+function main(data) {
+    for (let feed of data.feeds){
+        $('feeds').innerHTML += `<div class="card">
+        <header>
+        <a href=""><h3>${feed.name}</h3></a>
+        </header>
+        <div class="container">
+        <p>${feed.description?feed.description:''}</p>
+        </div>
+        <br>
+        </div>`
+    };
+    for (let lang of data.languages){
+        $('langs').innerHTML += `<option value="${lang[1]}">${lang[0]}</option>`;
+    };
+    for (let console of data.consoles){
+        $('consoles').innerHTML += `<option value="${console}">${console}</option>`;
+    };
+    for (let i of BOTS.nv){
+        $('nv').innerHTML += `<option value="${i}">${i}</option>`;
+    };
+    for (let i of BOTS.fd){
+        $('fd').innerHTML += `<option value="${i}">${i}</option>`;
+    }
+    for (let i of BOTS.tr){
+        $('tr').innerHTML += `<option value="${i}">${i}</option>`;
+    }
+}
+onload = () => {
+    fetch("assets/json/prototype.json")
+        .then(res => res.json())
+        .then(json => main(json));
+}
